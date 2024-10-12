a7e5ec5df20ad92a2cef44eb97a6b5325e06b9d1
kasumov <d.kasumov@comparus.de>
2024-10-12 23:33:14 +0300

    one more fix

--- src/app/ngsw-worker.js
+++ src/app/ngsw-worker.js
@@ -3,22 +3,22 @@ self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', fetchEvent => {
-  const url = new URL(fetchEvent.request.url);
-
-  console.log('fetch', url.pathname, url.searchParams, fetchEvent.request.method);
-
-  if (url.searchParams.has('share-target') && fetchEvent.request.method === 'POST') {
-    return fetchEvent.respondWith(async () => {
-      const formData = await fetchEvent.request.formData();
-
-      const keys = await caches.keys();
-      const sharedCache = await caches.open(
-          keys.filter((key) => key.startsWith('share-target'))[0]
-      );
-
-      await sharedCache.put('shared-data', new Response(formData));
-      return new Response.redirect('./?share-target', 303);
-    });
-  }
-});
+// self.addEventListener('fetch', fetchEvent => {
+//   const url = new URL(fetchEvent.request.url);
+//
+//   console.log('fetch', url.pathname, url.searchParams, fetchEvent.request.method);
+//
+//   if (url.searchParams.has('share-target') && fetchEvent.request.method === 'POST') {
+//     return fetchEvent.respondWith(async () => {
+//       const formData = await fetchEvent.request.formData();
+//
+//       const keys = await caches.keys();
+//       const sharedCache = await caches.open(
+//           keys.filter((key) => key.startsWith('share-target'))[0]
+//       );
+//
+//       await sharedCache.put('shared-data', new Response(formData));
+//       return new Response.redirect('./?share-target', 303);
+//     });
+//   }
+// });
